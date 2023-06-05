0c87c0fdb0800d62c9420438ee04c015757abb0e
Igor Helio Martins de Carvalho <iggor1935@hotmail.com>
2023-06-05 12:24:02 -0300

    Updated auva_bd.sql

--- bd_auva.sql
+++ bd_auva.sql
@@ -7,7 +7,7 @@ id int auto_increment primary key,
 nome varchar(100)
 );
 
-drop table tb_funcionario
+
 create table tb_funcionario(
 id int auto_increment primary key,
 nome varchar(300),
@@ -15,7 +15,7 @@ valor_hora DECIMAL(6,2),
 data_importacao datetime
 );
 
-drop table tb_ponto_funcionario
+
 create table tb_ponto_funcionario(
 id int auto_increment primary key,
 entrada timestamp,
@@ -27,10 +27,6 @@ CONSTRAINT id_func_fk foreign key(id_funcionario)
 References tb_funcionario(id)
 );
 
-select id as Codigo, nome as Nome, valor_hora as ValorHora, data_importacao as DataImportacao from tb_funcionario WHERE id = 1
 
-select * from tb_funcionario
-
-insert into tb_funcionario (nome, valor_hora, data_importacao) values (nome,valor_hora, data_importacao)
 
 
